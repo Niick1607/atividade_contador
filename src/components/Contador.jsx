dde3b082379a344b23fab39fb689ccf40ba0f890
Niick_1607 <105996755+Niick1607@users.noreply.github.com>
2024-07-08 22:45:45 -0300

    commit2

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 function Contador() {
     const [count, setCounter] = useState(0);
-    
+    const [i, setIndex] = useState(0);
+    const text = ["Texto1", "Texto2"];
+
+    const changeCounter = () => {
+        setCounter(count + 1);
+    };
+
+    const changeText = () => {
+        setIndex((previ) => (previ === 1 ? 0 : 1));
+    };
+
     return (
         <section className="container">
             <h1>{count}</h1>
-            <button onClick={setCounter(count +1)}>Click!</button>
+            <button onClick={changeCounter}>Contador!</button>
+            <h1>{text[i]}</h1>
+            <button onClick={changeText}>Click!</button>
         </section>
     );
 }
