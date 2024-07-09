9c00ee6afa53fd95b795ba0b24011592148c23ea
Niick_1607 <105996755+Niick1607@users.noreply.github.com>
2024-07-08 22:26:51 -0300

    Initial commit

---
+++ src/components/Contador.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Contador() {
+    const [count, setCounter] = useState(0);
+    return (
+        <section className="container">
+            <h1>{count}</h1>
+            <button onClick={setCounter(count +1)}>Click!</button>
+        </section>
+    );
+}
+
+export default Contador;
